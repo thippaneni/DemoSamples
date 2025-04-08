b989e952e4f84e46f4815d46ef59fa3621a9bd6c
thippaneni <thippaneni@gmail.com>
2025-04-08 07:34:20 +0530

    RecordVsClass Example

---
+++ RecordVsClass/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordVsClass
+{
+    // Record Example
+    public record Person(string Name, int Age); // Immutable, value-based equality
+}
---
+++ RecordVsClass/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RecordVsClass;
+
+Console.WriteLine("Wlcome to Record Vs Class Demo");
+
+var person1 = new Person("John", 21);
+var person2 = new Person("John", 21);
+//var person3 = person1 with { Age = 30 };
+
+var customer1 = new Customer { Name = "John", Age = 21 };
+var customer2 = new Customer { Name = "John", Age = 21 };
+
+Console.WriteLine($"Person1 == Person2: {person1 == person2}"); // True, value-based equality
+Console.WriteLine($"Customer1 == Customer2: {customer1 == customer2}"); // False, reference-based equality
